45636b62266dfa51ad399af22965add16207907e
DulanjiAbeysekara <dulanjiabeysekara09@gmail.com>
2024-07-01 22:46:50 +0530

    creating api gateway

---
+++ API-GateWay/src/main/java/lk/ijse/gdse66/API_GateWay/ApiGateWayApplication.java
@@ -0,0 +1,15 @@
+package lk.ijse.gdse66.API_GateWay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ApiGateWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGateWayApplication.class, args);
+	}
+
+}
---
+++ API-GateWay/src/test/java/lk/ijse/gdse66/API_GateWay/ApiGateWayApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse66.API_GateWay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGateWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
