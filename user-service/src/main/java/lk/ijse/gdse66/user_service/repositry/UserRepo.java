b5b61895e3acb72612dfd82b5827dd8560526013
DulanjiAbeysekara <dulanjiabeysekara09@gmail.com>
2024-07-02 14:23:27 +0530

    complete user repo class

--- user-service/src/main/java/lk/ijse/gdse66/user_service/repositry/UserRepo.java
+++ user-service/src/main/java/lk/ijse/gdse66/user_service/repositry/UserRepo.java
@@ -1,5 +1,8 @@
 package lk.ijse.gdse66.user_service.repositry;
 
-public interface UserRepo {
+import lk.ijse.gdse66.user_service.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User,Long> {
 }
 
